ed4894c89a162b86efd001ff711d77bb80f78992
mau3009 <mauchi3009@gmail.com>
2025-05-25 20:30:42 -0600

    Lab. 09 Condiciones

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hola");
+Console.WriteLine("Ingrese una hora del día");
+
+if (int.TryParse(Console.ReadLine(), out int hora) && hora >= 0 && hora <= 23)
+{
+    if (hora >=0 && hora <=11)
+    {
+        Console.WriteLine("Buenos días");
+    }
+    else if (hora >= 12 && hora <= 18)
+    {
+        Console.WriteLine("Buenas tardes");
+    }
+    else if  (hora >= 19 && hora <=23)
+    {
+        Console.WriteLine("Buenas noches");
+    }
+}
