2770e3f75ee47f8a372c6cdf6650ee93ed8fbc83
mau3009 <mauchi3009@gmail.com>
2025-05-25 20:32:42 -0600

    Lab. 08

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
-
-Console.WriteLine("Hola");
-Console.WriteLine("Ingrese una hora del día");
-
-if (int.TryParse(Console.ReadLine(), out int hora) && hora >= 0 && hora <= 23)
-{
-    if (hora >=0 && hora <=11)
-    {
-        Console.WriteLine("Buenos días");
-    }
-    else if (hora >= 12 && hora <= 18)
-    {
-        Console.WriteLine("Buenas tardes");
-    }
-    else if  (hora >= 19 && hora <=23)
-    {
-        Console.WriteLine("Buenas noches");
-    }
-}
+Console.WriteLine("Bienvenido");
+Console.WriteLine("Ingresa tu nombre completo");
+String nombre; 
+nombre = Console.ReadLine();
+Console.WriteLine("Su nombre completo es: " + nombre);
+Console.WriteLine("Ingrese su altura en metros");
+decimal altura;
+altura=Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Su altura es de: " + altura);
+Console.WriteLine("Ingrese su edad");
+int edad;
+edad = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Su edad es: " + edad);
+Console.WriteLine("Ingrese la incicial de su mascota");
+char mascota;
+mascota = Convert.ToChar (Console.ReadLine());
+Console.WriteLine("La inicial de su mascota es: " + mascota);
+Console.WriteLine("Todos los datos son:");
+Console.WriteLine(nombre);
+Console.WriteLine(altura);
+Console.WriteLine(edad);
+Console.WriteLine(mascota);
