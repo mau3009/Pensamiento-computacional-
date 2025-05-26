406bba18f4d8eaf5b46b0ca078240d883aa54058
mau3009 <mauchi3009@gmail.com>
2025-05-25 20:54:49 -0600

    Lab.12

---
+++ cadenas.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Ingrese un texto: ");
+string texto = Console.ReadLine();
+
+String [] palabras = texto.Split(new char []{' '}, StringSplitOptions.RemoveEmptyEntries);
+int cantidadPalabras = palabras.Length;
+
+TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+string textoCapitalizado = textInfo.ToTitleCase(texto.ToLower());
+
+Console.WriteLine($"\ncantidad de palabras ingresadas: {cantidadPalabras}");
+Console.WriteLine();
+Console.WriteLine("Texto con la primera letra de cada palabra en mayúscula: ");
+Console.Write(textoCapitalizado);
